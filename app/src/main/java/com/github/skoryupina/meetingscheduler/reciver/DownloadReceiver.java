19fd3a1eb5617b4a2b51f38473d6f05841fb1d37
skoryupina <e.skoryupina.develop@ya.ru>
2015-12-18 17:52:17 +0400

    Start working on background receiver logic and rest service for mobile service. Added methods onReceive() for BackgroundReceiver, auth check was based on shared preferences file, where user login and password are expected to be. DownloadReceiver contains methods for receiving result from MeetingRestClientService.

---
+++ app/src/main/java/com/github/skoryupina/meetingscheduler/reciver/DownloadReceiver.java
@@ -0,0 +1,28 @@
+package com.github.skoryupina.meetingscheduler.reciver;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+
+public class DownloadReceiver  extends ResultReceiver {
+    private Receiver mReceiver;
+
+    public DownloadReceiver(Handler handler) {
+        super(handler);
+    }
+
+    public void setReceiver(Receiver receiver) {
+        mReceiver = receiver;
+    }
+
+    public interface Receiver {
+        void onReceiveResult(int resultCode, Bundle resultData);
+    }
+
+    @Override
+    protected void onReceiveResult(int resultCode, Bundle resultData) {
+        if (mReceiver != null) {
+            mReceiver.onReceiveResult(resultCode, resultData);
+        }
+    }
+}
---
+++ app/src/main/java/com/github/skoryupina/meetingscheduler/reciver/MeetingRestClientService.java
@@ -0,0 +1,4 @@
+package com.github.skoryupina.meetingscheduler.reciver;
+
+public class MeetingRestClientService {
+}
